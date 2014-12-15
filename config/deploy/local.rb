f420c98988cda99cca11b57d4d2a606f810c7179
soar <85624529@qq.com>
2014-12-15 17:51:50 +0800

    change config.rb using relative url

--- config/deploy/local.rb
+++ config/deploy/local.rb
@@ -5,9 +5,9 @@ set :branch, "master"
 
 set :log_level, :debug
 
-set :deploy_to, "/home/soar/site/legend"
+set :deploy_to, "/home/soar/site/legend_site"
 
-set :build_folder, "/home/soar/site/legend"
+set :build_folder, "/home/soar/site/legend_site/build"
 set :site_folder, "/home/soar/site/legend"
 #set :password, ask('Server password', nil)
 ##server fetch(:server_name), user: 'soar', port: 22, password: fetch(:password), roles: %w{web app db}
