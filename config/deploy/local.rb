e31178da808d3a8517a411def4b6cffc3c4bd792
soar <85624529@qq.com>
2014-12-15 15:54:45 +0800

    change local cap

--- config/deploy/local.rb
+++ config/deploy/local.rb
@@ -12,4 +12,4 @@ set :site_folder, "/home/soar/site/legend"
 #set :password, ask('Server password', nil)
 ##server fetch(:server_name), user: 'soar', port: 22, password: fetch(:password), roles: %w{web app db}
 #
-#server fetch(:server_name), user: "houjn", roles: %w{web app db}
+server fetch(:server_name), user: "soar", roles: %w{web app db}
